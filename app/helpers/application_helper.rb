5b2edff99a6c276174a220cb94dd2fd61a738f69
David Maurer <davemaurermail@gmail.com>
2015-08-19 15:44:24 -0600

    added guard file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      title
+    else
+      "#{page_title} | #{title}"
+    end
+  end
 end
