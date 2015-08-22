4ac6aaa7dfef0177194e40f3c7521391a62ab62e
David Maurer <davemaurermail@gmail.com>
2015-08-22 14:58:24 -0600

    implemented user login and current_user

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,9 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   fixtures :all
+
+  #returns true if a test user is logged in
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
